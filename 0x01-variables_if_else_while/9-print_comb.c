a01720b35ee9f4b7dd7974b6dae80d828dbc44a4
PaaNii93 <nuertere@yahoo.com>
2022-10-20 07:46:03 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: 0 (success)
+ **/
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i == '9')
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
