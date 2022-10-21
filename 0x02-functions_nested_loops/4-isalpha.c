1ecc13514e6bf4114b9ca869de2d64c305b69ed0
PaaNii93 <nuertere@yahoo.com>
2022-10-21 09:49:52 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - entry point.
+ * Description: Return 1 if a is a  letter, both lowercase and uppercase.
+ * @a: is unassigned int to be compared with ASCII value.
+ * Return: 0 (success)
+ **/
+
+int _isalpha(int a)
+{
+	if (a >= 65 && a <= 122)
+		return (1);
+	else
+		return (o);
+}
