5cf2baeae8c3211f05be0fa2b7e1960788426026
PaaNii93 <nuertere@yahoo.com>
2022-10-24 12:56:01 -0700

    11

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,17 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * swap_int - swap values of two integers.
- * @a: a pointer deference for a
- * @b: a pointer deference for b
- * int c: assigning the address of variable a to c
- * Return: 0 (success)
- */
+ * swap_int - This function swaps the value of two integers
+ * @a: a pointer dereference for changing the value of the variable a
+ * @b: a pointer deference for changing the value of the variable b
+ * int c: assigning the address of the variable a to c
+ * Return 0 success.
+*/
 
 void swap_int(int *a, int *b)
 {
-	int c = *b
-	*a = *b
-	*a = c
+	int c = *a;
+	*a = *b;
+	*b = c;
 }
