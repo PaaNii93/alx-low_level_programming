6195bf08233e7c0445f10ed8cf6865ff1d83323a
PaaNii93 <nuertere@yahoo.com>
2022-11-02 03:32:14 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - function compares two strings.
+ * @s1: string1
+ * @s2: string2
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+	return (0);
+}
