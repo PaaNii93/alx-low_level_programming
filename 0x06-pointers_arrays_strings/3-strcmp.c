b041c7df0ba20b6a89cf596d03f2cd361356719b
PaaNii93 <nuertere@yahoo.com>
2022-11-03 11:17:15 -0700

    333

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,28 +5,16 @@
  * _strcmp - function compares two strings.
  * @s1: string 1
  * @s2: string 2
- * Return: 0.
+ * Return: a
  */
 
 int _strcmp(char *s1, char *s2)
-int _putchar(int *a)
 
-{	
+{
+	int a;
+
 	a = strcmp(s1, s2);
 
-	if (a < 0)
-	{
-		putchar("s1 is less than s2");
-	}
-	else if (a > 0)
-	{
-		putchar("s1 is greater than s2");
-	}
-	else 
-	{
-		putchar("s1 iss equal to s2");
-	}
-
-	return (0);
+	return (a);
 }
 
